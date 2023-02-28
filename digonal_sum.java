9368684dec490319a62fc47a6165054ceadff244
Amit0416 <126446313+Amit0416@users.noreply.github.com>
2023-03-01 00:36:55 +0530

    Add files via upload

---
+++ digonal_sum.java
@@ -0,0 +1,26 @@
+public class digonal_sum {
+    public static int digonalSum(int matrix[][]) {
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            // pd
+            sum += matrix[i][i];
+            // sd
+            if (i != matrix.length - 1 - i)
+                sum += matrix[i][matrix.length - i - 1];
+        }
+        return sum;
+    }
+
+    /**
+     * @param aegs
+     */
+    public static void main(String aegs[]) {
+        final int matrix[][] = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 }
+        };
+        System.out.println(digonalSum(matrix));
+
+    }
+}
