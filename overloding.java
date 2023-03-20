c3a7ef32c2ca3895e8d87c45112c94c7700b6aad
Amit0416 <126446313+Amit0416@users.noreply.github.com>
2023-03-21 00:52:33 +0530

    Add files via upload

---
+++ overloding.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class overloding {
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(sum(3, 7));
+        System.out.println(sum(3, 5, 6));
+    }
+
+}
